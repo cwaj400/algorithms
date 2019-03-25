ca8bc08872614beecc07ade6e58c3d6ba1eea5aa
Will Angell-James <william.angelljames9@gmail.com>
2019-03-25 11:23:10 -0400

    adding sock matching

---
+++ Sock-matching/Sock-matching.java
@@ -0,0 +1,33 @@
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+
+  }
+
+  // Complete the sockMerchant function below.
+  public static int sockMerchant(int n, int[] ar) {
+
+
+    int result = 0;
+
+    Set map = new HashSet(n);
+    Iterator iterator = map.iterator();
+
+    for (int i = 0; i < n; i++) {
+      if (map.contains(ar[i])) {
+        result++;
+        map.remove(ar[i]);
+      } else {
+        map.add(ar[i]);
+      }
+    }
+
+    return result;
+  }
+}
